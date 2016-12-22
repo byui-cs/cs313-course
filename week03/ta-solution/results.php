4a465dcd1c9884cebf630e1ae9de012385474054
Scott Burton <scotthburton@gmail.com>
2016-12-22 11:16:25 -0700

    Adding TA Solution

--- week03/ta-solution/results.php
+++ week03/ta-solution/results.php
@@ -1,4 +1,47 @@
+<?php
+// First let's process all the input
+
+// using constants for the names of the elements in the form would be better...
+
+// It would also be better to use an ID of some sort for the
+// value that is submitted such as "cs" as opposed to "Computer Science",
+// then in PHP we could process that value and determine the exact
+// presentation text to render.
+$name = htmlspecialchars($_POST["name"]);
+$email = htmlspecialchars($_POST["email"]);
+$major = htmlspecialchars($_POST["major"]);
+$places = $_POST["places"];
+$comments = htmlspecialchars($_POST["comments"]);
+
+?>
 <!DOCTYPE html>
 <html>
+<head>
+	<title>Submission Results</title>
+</head>
+
+<body>
+	<h1>Submission Results</h1>
+
+	<p>Your name is: <?=$name ?></p>
+	<p>Your email is: <a href="mailto:<?=$email ?>"><?=$email ?></a></p>
+	<p>Your major is: <?=$major ?></p>
+	<p>You have been to the following places:</p>
+	<ul>
+
+<?
+foreach ($places as $place)
+{
+	$place_clean = htmlspecialchars($place);
+	echo "<li><p>$place_clean</p></li>";
+}
+?>		
+
+	</ul>
+
+	<p>Comments: <?=$comments?></p>
+
+</body>
+
 
 </html>
