9b6c07efd48b6ce2cb6ac9948832d0c446486e97
Scott Burton <scotthburton@gmail.com>
2017-02-01 10:41:27 -0700

    Added command to tell PDO to generate exceptions on errors

--- week05/ta-solution/heroku_and_local_example/dbConnect.php
+++ week05/ta-solution/heroku_and_local_example/dbConnect.php
@@ -43,6 +43,8 @@ function get_db() {
 		// Create the PDO connection
 		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
+		// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 	}
 	catch (PDOException $ex) {
 		// If this were in production, you would not want to echo
--- week05/ta-solution/viewScriptures.php
+++ week05/ta-solution/viewScriptures.php
@@ -19,6 +19,8 @@ try
 	// Create the PDO connection
 	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
+	// this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+	$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 }
 catch (PDOException $ex)
 {
