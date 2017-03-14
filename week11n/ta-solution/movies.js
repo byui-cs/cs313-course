c652e16ff2fccda8507ff14ff0c3f46cd66b9afd
Scott Burton <scotthburton@gmail.com>
2017-03-14 09:34:20 -0600

    Added week 11n team activity

---
+++ week11n/ta-solution/movies.js
@@ -0,0 +1,31 @@
+function search() {
+	// Get the value from the search box
+	var searchString = $("#txtSearch").val();
+	console.log("Searching for: " + searchString);
+
+	// Set up the parameters to send to the API
+	var params = {s: searchString};
+
+	// Use jQuery to make the get request
+	$.get("http://www.omdbapi.com/", params, function(data, status){
+		// For debugging purposes, make a note that we're back
+		console.log("Back from server with the following results:")
+		console.log(status);
+    	console.log(data);
+
+    	updateResultList(data)
+	});
+}
+
+function updateResultList(data) {
+	if (data.Search && data.Search.length > 0) {
+		var resultList = $("#ulResults");
+		resultList.empty();
+
+		for (var i = 0; i < data.Search.length; i++) {
+			var title = data.Search[i].Title;
+			resultList.append("<li><p>" + title + "</p></li>");
+		}
+	}
+
+}
