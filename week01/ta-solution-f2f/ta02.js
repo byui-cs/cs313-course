aaa72eaec3b1ec6b3a45d279e4cf8a8877c396ae
Scott Burton <scotthburton@gmail.com>
2017-04-19 10:42:10 -0600

    The F2F Class will be doing the client side activity during week 01

---
+++ week01/ta-solution-f2f/ta02.js
@@ -0,0 +1,17 @@
+// ta02.js
+function clickMe() {
+	alert("Clicked!");
+}
+
+function changeColor() {
+	var textbox_id = "txtColor";
+	var textbox = document.getElementById(textbox_id);
+
+	var div_id = "div1";
+	var div = document.getElementById(div_id);
+
+	// We should verify values here before we use them...
+	var color = textbox.value;
+	div.style.backgroundColor = color;
+
+}
