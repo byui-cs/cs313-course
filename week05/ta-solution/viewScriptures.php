de4af8c6f44c40f227e495fb9c74aa7fa388f924
Scott Burton <burtons@byui.edu>
2019-02-07 10:34:33 -0700

    Cleaned up the echo statement

--- week05/ta-solution/viewScriptures.php
+++ week05/ta-solution/viewScriptures.php
@@ -50,10 +50,12 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 	// The variable "row" now holds the complete record for that
 	// row, and we can access the different values based on their
 	// name
-	echo '<p>';
-	echo '<strong>' . $row['book'] . ' ' . $row['chapter'] . ':';
-	echo $row['verse'] . '</strong>' . ' - ' . $row['content'];
-	echo '</p>';
+	$book = $row['book'];
+	$chapter = $row['chapter'];
+	$verse = $row['verse'];
+	$content = $row['content'];
+
+	echo "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p>";
 }
 
 ?>
