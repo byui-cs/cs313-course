49e52a6adfdd7ae50678a63ce372f4b5c572d429
Scott Burton <burtons@byui.edu>
2019-03-05 08:48:37 -0700

    cleaned up the code a little more and added some additional comments

--- week09/ta-solution/index.js
+++ week09/ta-solution/index.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.set('port', (process.env.PORT || 5000));
-
+// tell it to use the public directory as one where static files live
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+// set up a rule that says requests to "/math" should be handled by the
+// handleMath function below
 app.get('/math', handleMath);
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+// start the server listening
+app.listen(port, function() {
+  console.log('Node app is running on port', port);
 });
+
+
+/**********************************************************************
+ * Ideally the functions below here would go into a different file
+ * but for ease of reading this example and seeing all of the pieces
+ * they are listed here.
+ **********************************************************************/
 
 function handleMath(request, response) {
 	const operation = request.query.operation;
